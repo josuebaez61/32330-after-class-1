7c0beededa430abb8107fa200b19d8be9aec825b
josue baez <josuebaez61@gmail.com>
2022-11-26 14:06:13 -0300

    CHANGE, layout folder path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
-import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { PageWrapperComponent } from './shared/layout/page-wrapper/page-wrapper.component';
+import { TopBarComponent } from './shared/layout/top-bar/top-bar.component';
 import { StudentsPageComponent } from './pages/students-page/students-page.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 
